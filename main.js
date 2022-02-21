f6714b4b1d4ddc8e874308cd3fce505802b76ae6
Laande <xerixexe@gmail.com>
2022-02-21 17:03:52 +0100

    ups

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ const init = function(){
   let ad_in_game = document.getElementsByClassName('roomenterad-habblet-container');
   let ad_cms = document.getElementsByClassName('adsbygoogle');
   let ad_bottom = document.getElementsByClassName('adsbygoogle adsbygoogle-noablate');
+  
+  if(ad_in_game.length > 0 || ad_cms.length > 0 || ad_bottom.length > 0){
+    console.log('[-] Ads removed');
+  }
 
-  if(ad_in_game){
+  if(ad_in_game.length > 0){
     while (ad_in_game.length > 0) ad_in_game[0].remove();
   }
-  if(ad_cms){
+  if(ad_cms.length > 0){
     while (ad_cms.length > 0) ad_cms[0].remove();
   }
-  if(ad_bottom){
+  if(ad_bottom.length > 0){
     while (ad_bottom.length > 0) ad_bottom[0].remove();
-  }
-
-  if(ad_in_game || ad_cms || ad_bottom){
-    console.log('[-] Ads removed');
   }
 }
 
