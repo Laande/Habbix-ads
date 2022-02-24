4a4169c299bcdd4700067477e8508dea5ef3384a
Laande <xerixexe@gmail.com>
2022-02-24 21:42:22 +0100

    remove new ad

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const init = function(){
   let ad_in_game = document.getElementsByClassName('roomenterad-habblet-container');
   let ad_cms = document.getElementsByClassName('adsbygoogle');
   let ad_bottom = document.getElementsByClassName('adsbygoogle adsbygoogle-noablate');
+  let ad_new = document.getElementsByClassName('google-auto-placed');
   
-  if(ad_in_game.length > 0 || ad_cms.length > 0 || ad_bottom.length > 0){
+  if(ad_in_game.length > 0 || ad_cms.length > 0 || ad_bottom.length > 0 || ad_new.length > 0){
     console.log('[-] Ads removed');
   }
 
@@ -15,6 +16,9 @@ const init = function(){
   }
   if(ad_bottom.length > 0){
     while (ad_bottom.length > 0) ad_bottom[0].remove();
+  }
+  if(ad_new.length > 0){
+    while (ad_new.length > 0) ad_new[0].remove();
   }
 }
 
