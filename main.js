9d5e8889782ab7b052d30cd52f71b9d95a53ad67
Laande <xerixexe@gmail.com>
2022-02-24 22:22:41 +0100

    better js

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
-const init = function(){
-  let ad_in_game = document.getElementsByClassName('roomenterad-habblet-container');
-  let ad_cms = document.getElementsByClassName('adsbygoogle');
-  let ad_bottom = document.getElementsByClassName('adsbygoogle adsbygoogle-noablate');
-  let ad_new = document.getElementsByClassName('google-auto-placed');
-  
-  if(ad_in_game.length > 0 || ad_cms.length > 0 || ad_bottom.length > 0 || ad_new.length > 0){
-    console.log('[-] Ads removed');
-  }
+let ads = [
+    document.getElementsByClassName('roomenterad-habblet-container'),
+    document.getElementsByClassName('adsbygoogle'),
+    document.getElementsByClassName('adsbygoogle adsbygoogle-noablate'),
+    document.getElementsByClassName('google-auto-placed')
+]
 
-  if(ad_in_game.length > 0){
-    while (ad_in_game.length > 0) ad_in_game[0].remove();
-  }
-  if(ad_cms.length > 0){
-    while (ad_cms.length > 0) ad_cms[0].remove();
-  }
-  if(ad_bottom.length > 0){
-    while (ad_bottom.length > 0) ad_bottom[0].remove();
-  }
-  if(ad_new.length > 0){
-    while (ad_new.length > 0) ad_new[0].remove();
-  }
+for (let i = 0; i < ads.length; i++) {
+    while (ads[i].length > 0) {
+        ads[i][0].remove();
+    }
 }
-
-init();
